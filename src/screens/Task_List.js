9a2223ed52daec934f6d953158ca934a6e095b43
Dantesszz <maria.santos2830@etec.sp.gov.br>
2024-05-21 10:42:15 -0300

    alterando em TaksList

--- src/screens/Task_List.js
+++ src/screens/Task_List.js
@@ -1,13 +1,20 @@
 import React, {Component} from "react" //import dos componentes
 import {StylesSheet, View, Text} from "react-native" //import do css, view e text
 
+import today_Image from "../../assets/imgs/today.jpg"
+
 // cria uma classe padrão que será usada em outras partes do programa
 export default class TaskList extends Component{ // tem todas as características de um componente (herança)
     render(){
         return(
-            <view style ={styles.container}>
-                <Text>Lista de Tarefas</Text>
-            </view>
+            <View style ={styles.container}>
+                <ImageBackground source={today_Image} style={styles.background}>
+                    
+                </ImageBackground>
+                <View style={styles.taskList}>
+
+                </View>
+            </View>
         )
     }
 } 
@@ -16,6 +23,12 @@ export default class TaskList extends Component{ // tem todas as característica
 const styles = StylesSheet.create({ //variável q guarda os estilos
     container: {
         flex: 1
+    },
+    backgroud:{
+        flex: 3
+    },
+    taskList:{
+        flex: 7
     }
 
 })
