e57025c4ac7c6ba73debb54cf898fe2ab46fd402
Dantesszz <maria.santos2830@etec.sp.gov.br>
2024-05-21 10:26:48 -0300

    TaskList

---
+++ src/screens/Task_List.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react" //import dos componentes
+import {StylesSheet, View, Text} from "react-native" //import do css, view e text
+
+// cria uma classe padrão que será usada em outras partes do programa
+export default class TaskList extends Component{ // tem todas as características de um componente (herança)
+    render(){
+        return(
+            <view style ={styles.container}>
+                <Text>Lista de Tarefas</Text>
+            </view>
+        )
+    }
+} 
+
+//esquema de css
+const styles = StylesSheet.create({ //variável q guarda os estilos
+    container: {
+        flex: 1
+    }
+
+})
