f2e1e417b0debb3d6d599fdc2d0a743e48b45f54
unknown <maria.santos2830@etec.sp.gov.br>
2024-09-20 09:41:14 -0300

    add_tarefas

---
+++ src/screens/Add_Tarefa.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import {
+    Text,
+    TextInput,
+    View,
+    StyleSheet,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    Modal //recurso de tela sobreposta; uma tela em cima da outra
+} from "react-native"
+
+export default class Add_Tarefa extends Component{
+    render(){
+        return(
+
+        )
+    }
+}
